766eaff8c0e5797afc94e7448a660d0e61ff759c
lowryel <eugeneagbaglo@gmai.com>
2023-01-16 09:28:47 +0000

    added databse form railway

--- church/settings.py
+++ church/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = "church.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config['name'],
-#         'USER': config['user'],
-#         'PASSWORD': config['pass'],
-#         'HOST': config['host'],
-#         'PORT': '5432'
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'R9zkXTg6v9ULtDh8JwDj',
+        'HOST': 'containers-us-west-116.railway.app',
+        'PORT': 7587, 
+    }
+}
 
 
 # Password validation
