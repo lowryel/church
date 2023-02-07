cdbc607d8ff44faec71c2fec0d048b0075bd552d
lowryel <eugeneagbaglo@gmail.com>
2023-02-07 11:48:43 +0000

    debug to false

--- church/settings.py
+++ church/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-gij&guvfg$i+res@p%t3^b#&z_)sbyd3nzuz1x-km!cc_k(doy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-34-201-164-155.compute-1.amazonaws.com']
 
