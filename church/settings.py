c46d0b9b2d62ff7080179ecf73b2a0fd78e933f0
lowryel <eugeneagbaglo@gmail.com>
2023-02-07 10:22:15 +0000

    added external host

--- church/settings.py
+++ church/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-gij&guvfg$i+res@p%t3^b#&z_)sbyd3nzuz1x-km!cc_k(doy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-34-201-164-155.compute-1.amazonaws.com']
 
 
 # Application definition
