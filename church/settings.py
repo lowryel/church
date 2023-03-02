a5a69c30891f9212d0a0992588e2ee21496d2763
lowryel <eugeneagbaglo@gmail.com>
2023-03-02 23:51:21 +0000

    added more fonts

--- church/settings.py
+++ church/settings.py
@@ -152,6 +152,18 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+FONTS = {
+    'default': {
+        'family': 'TT-Firs-Neue-Trial-Light',
+        'variant': 'regular',
+        'size': 16,
+        'color': '#000000',
+        'weight': 'normal',
+    },
+}
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
