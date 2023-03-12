7bb62b2cd1a85be2d1b2fa19d43488eed9f7c9c4
lowryel <eugeneagbaglo@gmail.com>
2023-03-12 21:36:24 +0000

    fix the s3 url issue

--- church/settings.py
+++ church/settings.py
@@ -65,7 +65,7 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+AWS_STORAGE_BUCKET_NAME = "bekoemedia"
 AWS_QUERYSTRING_AUTH = False # This will make sure that the file URL does not have unnecessary parameters like your access key.
 AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
 
@@ -181,7 +181,7 @@ USE_TZ = True
 STATIC_URL = 'https: //' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 MEDIA_URL = STATIC_URL + 'media/'
 STATICFILES_DIRS = [BASE_DIR, "church/static"]
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static'
 MEDIA_ROOT = 'media/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',
